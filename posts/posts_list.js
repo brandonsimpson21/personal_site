03533576cc8104e20b6357687411cc9f575ecd3b
Brandon <brandonsimpson21@gmail.com>
2021-01-07 14:56:07 -0700

    finish porting td auth and ohlc posts began live quotes post

--- posts/posts_list.js
+++ posts/posts_list.js
@@ -1,27 +1,35 @@
 posts = [{
     article: "evolutionary_agents",
     title: 'Evolutionary Agents',
-    date: '04/04/2019',
+    date: '01/01/2020',
     description: 'Evolutionary agent decision-making model',
     image: 'Images/EAS.png',
     path: 'posts/evolutionary_agents/evolutionary_agents.ejs'
 
     },
-    {
-        article: "TD_Auth",
-        title: 'TD Ameritrade Authorization',
-        date:'03/01/2019',
-        description: 'How to get a Td Ameritrade authorization token',
-        image: '#',
-        path: 'posts/td_auth/td_auth.ejs'
-    },
+    // {
+    //     article: "live_quotes",
+    //     title: 'TD Ameritrade Live Quotes',
+    //     date:'12/20/2019',
+    //     description: 'Get Live Quotes From TD Ameritrade In Node',
+    //     image: 'Images/live_card_cap.png',
+    //     path: 'posts/live_quotes/live_quotes.ejs'
+    // },
     {
         article: "td_utils_OHLC",
         title: 'TD Ameritrade Utilities and OHLC',
-        date:'03/01/2019',
+        date:'12/05/2019',
         description: 'TD Ameritrade Utilities and Pulling OHLC Data',
-        image: '#',
+        image: 'Images/OHLC_card_cap.jpg',
         path: 'posts/td_utils_OHLC/td_utils_OHLC.ejs'
+    },
+    {
+        article: "TD_Auth",
+        title: 'TD Ameritrade Authorization',
+        date:'12/01/2019',
+        description: 'How to get a Td Ameritrade authorization token',
+        image: 'Images/key.jpg',
+        path: 'posts/td_auth/td_auth.ejs'
     }
 ]
 
