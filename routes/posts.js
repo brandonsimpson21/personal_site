775a8bea185b57a24737b5e89f3916bf3e9ea2ed
Brandon <brandonsimpson21@gmail.com>
2021-01-24 12:18:35 -0700

    moved testing dependencies to dev dependencies

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 
-const posts = require('../posts/posts_list') //TODO put posts in database
+const posts = require('../posts/posts_list')
 
 
 router.get('/', (req, res) => {
